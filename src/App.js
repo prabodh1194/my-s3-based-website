645674bb70d88246610025749500a0345e3b898a
Prabodh Agarwal <prabodh1194@gmail.com>
2023-12-04 23:38:10 +0530

    add a router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import logo from './logo.svg';
-import logoS3 from './logoS3.png';
-import logoCF from './logoCloudFront.png';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-MainLogo" alt="logo"/>
-            </header>
             <p>This react-based application is hosted in an S3 bucket exposed through a CloudFront distribution</p>
-            <div className="logos">
-                <img src={logoS3} className="App-logoR2L" alt="logo S3"/>
-                <img src={logoCF} className="App-logoL2R" alt="logo CloudFront"/>
-            </div>
+            <Router>
+                <Switch>
+                    <Route exact path="/a" render={(props) => <span>a</span>}/>
+                    <Route exact path="/b" render={(props) => <span>b</span>}/>
+                    <Route exact path="/c" render={(props) => <span>c</span>}/>
+                </Switch>
+            </Router>
         </div>
     );
 }
