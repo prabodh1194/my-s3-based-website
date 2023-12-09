917b990dd2f3f81ed80bcd69c2234ca398cda768
Prabodh Agarwal <prabodh1194@gmail.com>
2023-12-10 00:42:55 +0530

    add CSS for navbar

--- src/Top.js
+++ src/Top.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import './navbar.css'
+
 class Top extends Component {
 
     render() {
         return (
             <header className="masthead mb-auto">
-                <div className="container">
+                <div className="navbar">
                     <Link to="/" key="btn-hm">
                         <img
-                            className="top bt nunito img"
+                            className="navicon"
                             alt="home"
                             src="/favicon.ico" />
                     </Link>
+                    <div className="nav-buttons">
                     {["Blogger", "About"].map((el, idx) =>
                     {
                         return (<Link
                                 to={`/${el.toLowerCase()}`}
                                 key={`btn-${idx}`}>
-                                <button className="top bt nunito text">
+                                <button className="top bt nunito text nav-button">
                                     {el}
                                 </button>
                             </Link>
                         );
                     })
                     }
+                    </div>
                 </div>
             </header>
         );
