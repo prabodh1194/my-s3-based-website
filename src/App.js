1690c583d7640f0ad03581dc22687b4423ebead5
Prabodh Agarwal <prabodh1194@gmail.com>
2023-12-10 22:21:37 +0530

    create darkmode toggler

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Top from './Top';
 import Main from './Main';
+import ToggleDarkMode from "./ToggleDarkMode";
 
 class App extends Component {
     render() {
         return (
             <Router>
                 <div>
+                    <ToggleDarkMode />
+                    <input type="checkbox" name="light-switch" className="light-switch" />
+                    <label htmlFor="light-switch">Switch to light / dark version</label>
                     <Route path="/" render={(props) => <Top location={props.location}/>} />
                     <Switch>
                         <Route exact path="/" render={(props) => <Main {...props} />} />
