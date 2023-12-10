e8f69324c11c71cf521c52d1b8b1a38300f8a0f1
Prabodh Agarwal <prabodh1194@gmail.com>
2023-12-10 21:55:22 +0530

    add tailwind css

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,14 @@
 import {Component} from "react";
 
 class Main extends Component {
-        render() {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-12 col-md-12 col-lg-12">
-                            <h1 className="text-center nunito">Hi, I'm <span className="name">Pbd</span></h1>
-                            <h2 className="text-center nunito">I'm a software engineer</h2>
-                        </div>
-                    </div>
-                </div>
-            );
-        }
+    render() {
+        return (
+            <div>
+                <h1 className="text-3xl font-bold underline">Hi, I'm <span>Pbd</span></h1>
+                <h2>I'm a software engineer</h2>
+            </div>
+        );
+    }
 }
 
 export default Main;
