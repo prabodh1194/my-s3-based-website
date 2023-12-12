6e87bf2f42ce35c0e922e2b6633852359dffb528
Prabodh Agarwal <prabodh1194@gmail.com>
2023-12-12 17:56:02 +0530

    set line-height

--- src/Main.js
+++ src/Main.js
@@ -5,7 +5,7 @@ class Main extends Component {
         return (
             <div className="row justify-content-evenly align-items-center h-75">
                 <div className="col-auto">
-                    <h1 style={{"color": "var(--bs-gray-700)"}}>Hi, I'm <span>Prabodh;</span></h1>
+                    <h1 style={{"color": "var(--bs-gray-700)", "lineHeight": "1.0"}}>Hi, I'm <span>Prabodh;</span></h1>
                     <h4 className="fw-light" style={{"color": "var(--bs-gray)"}}>you can call me <span
                         className="fst-italic">pbd</span>.</h4>
                     <br/>
