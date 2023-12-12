f4b5883314396fffdf482181c56e47b134a765d7
Prabodh Agarwal <prabodh1194@gmail.com>
2023-12-12 17:52:29 +0530

    fix link

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
                     <Route element={<Top/>}>
                         <Route exact path="/" element={<Main/>}/>
                         <Route exact path="/about" element={<div>a</div>}/>
-                        <Route exact path="/blogger" element={<div>b</div>}/>
+                        <Route exact path="/blog" element={<div>b</div>}/>
                         <Route exact path="/resume"
                                element={() => window.location = 'https://drive.google.com/open?id=1dwvo4DMUiaBLmgXu1QsH5ipHtCaogrSU'}/>
                     </Route>
