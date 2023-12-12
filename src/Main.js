acde4f40e00aaa02ffd4a69b5a8aafe6013578e6
Prabodh Agarwal <prabodh1194@gmail.com>
2023-12-12 10:57:29 +0530

    customise front page

--- src/Main.js
+++ src/Main.js
@@ -3,9 +3,12 @@ import {Component} from "react";
 class Main extends Component {
     render() {
         return (
-            <div>
-                <h1>Hi, I'm <span>Pbd</span></h1>
-                <h2>I'm a software engineer</h2>
+            <div className="row justify-content-evenly align-items-center vh-100">
+                <div className="col-auto">
+                    <h1>Hi, I'm <span>Prabodh</span></h1>
+                    <h4 className="fw-light" style={{"color": "var(--bs-gray)"}}>You can call me <span className="fst-italic">pbd</span></h4>
+                    <h2 className="font-monospace" style={{"color": "var(--bs-blue)"}}>I'm a software engineer</h2>
+                </div>
             </div>
         );
     }
