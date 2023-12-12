4ed6b9457f67d44d3c3cb9d08203edd3c9f87b87
Prabodh Agarwal <prabodh1194@gmail.com>
2023-12-12 10:30:23 +0530

    build bootstrap!

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
+import {Container} from "react-bootstrap";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Container>
+            <App/>
+        </Container>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
