65394add44f051fa477bbf7bf18411d9278c86ef
Prabodh Agarwal <prabodh1194@gmail.com>
2023-12-12 18:25:48 +0530

    fix about page

--- src/About.js
+++ src/About.js
@@ -1,9 +1,40 @@
 const About = () => {
     return (
         <div className="row justify-content-evenly align-items-center h-75">
-            a
+            <div className="col-6 fs-5 fw-light">
+                <h1 className="align left">Hi Peeps,</h1>
+                <content className="align left">
+                    <p>
+                        Prabodh Agarwal is an engineer by qualification whose
+                        certificate reads integrated dual degree in M.Sc.(Hons.)
+                        Biological Sciences and B.E.(Hons.) Computer Science.
+                        Only BITS Pilani awards such amazing degrees. You should
+                        definitely visit the sleepy hamlet of Pilani to
+                        understand what is going on there.
+                    </p>
+                    <p>
+                        I'm employing my <em>"skills"</em> as a software
+                        engineer while being employed by D. E. Shaw India Pvt.
+                        Ltd., Hyderabad, India.
+                    </p>
+                    <p>
+                        Doesn't our intermediate program require us to choose
+                        between one of these three. Well, now that I'm an
+                        engineer, I need to decide what to do with my life.
+                    </p>
+                    <p>
+                        My exposure to finance has been through my professional
+                        life and the tax deduction section of my monthly
+                        payslip. And hatred of the latter have exposed me to
+                        outside world. It is an interesting journey that I want
+                        to embark on and I'll let this blog do the talking.
+                    </p>
+                    <p>
+                        Happy browsing!
+                    </p>
+                </content>
+            </div>
         </div>
-    )
+    );
 }
-
 export default About;
