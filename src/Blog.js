0db0e32ed2418791986ab138c033551fa58afb39
Prabodh Agarwal <prabodh1194@gmail.com>
2023-12-16 00:58:48 +0530

    improve date wise rendering

--- src/Blog.js
+++ src/Blog.js
@@ -4,7 +4,7 @@ import React, {useEffect} from "react";
 const ArticleLine = (props) => {
     return (
         <div className="d-flex justify-content-between">
-            <span>{props.title}</span>
+            <span><a href={props.path}>{props.title}</a></span>
             <hr />
             <span>{props.created_on}</span>
         </div>
@@ -23,18 +23,36 @@ const Blog = (props) => {
 
     useEffect(() => {
         const _routes = blogs.map((Component, _) => {
+            const _created_date = new Date(Component.created_on);
+
+            if (z.year && _created_date.getFullYear() !== +z.year) {
+                return null;
+            }
+
+            if (z.month && 1 + _created_date.getMonth() !== +z.month) {
+                return null;
+            }
+
+            if (z.date && _created_date.getDate() !== +z.date) {
+                return null;
+            }
+
+            const path = `/blog/${_created_date.getFullYear()}/${1 + _created_date.getMonth()}/${_created_date.getDate()}/${Component.name}`
             return <ArticleLine title={Component.name}
+                                path={path}
                                 created_on={new Date(Component.created_on).toISOString().slice(0, 10)}/>
+        }).filter((x) => {
+            return x !== null
         })
         setRoutes(_routes)
-    }, [blogs]);
+    }, [blogs, z]);
 
 
     return (
         <>
             <h3>Articles:</h3>
             <div >
-                {routes}
+                {routes.length > 0 ? routes : <span>No articles found</span>}
             </div>
         </>
     )
