4a301f178380847ab46e168cee64b53d648c4122
Prabodh Agarwal <prabodh1194@gmail.com>
2023-12-16 12:43:47 +0530

    add a blog page

--- src/Blog.js
+++ src/Blog.js
@@ -1,21 +1,19 @@
-import {useParams, useSearchParams} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import React, {useEffect} from "react";
 
 const PAGE_SIZE = 15;
 
-const ArticleLine = (props) => {
-    return (
-        <div className="d-flex justify-content-between align-items-center">
-            <span><a href={props.path}>{props.title}</a></span>
-            <hr className="flex-grow-1 mx-2"/>
-            <time dateTime={props.created_on}
-                  style={{"width": "10ch", "fontVariantNumeric": "tabular-nums"}}>{props.created_on}</time>
-        </div>
-    )
-}
-const Blog = (props) => {
-    let z = useParams();
-    let [query, setSearchParams] = useSearchParams();
+const ArticleLine = (props) => (
+    <div className="d-flex justify-content-between align-items-center">
+        <span><a href={props.path}>{props.title}</a></span>
+        <hr className="flex-grow-1 ms-2 me-3"/>
+        <time dateTime={props.created_on}
+              style={{"width": "10ch", "fontVariantNumeric": "tabular-nums"}}>{props.created_on}</time>
+    </div>
+);
+
+const Blog = () => {
+    let pathParams = useParams();
 
     const [blogs, setBlogs] = React.useState([])
     const [routes, setRoutes] = React.useState([])
@@ -27,40 +25,45 @@ const Blog = (props) => {
     }, []);
 
     useEffect(() => {
-        const page = query.get("page") || 1;
         const _routes = blogs.map((Component, _) => {
             const _created_date = Component.created_on;
 
-            if (z.year && _created_date.year !== z.year) {
+            if (pathParams.year && _created_date.year !== pathParams.year) {
                 return null;
             }
 
-            if (z.month && 1 + _created_date.month !== z.month) {
+            if (pathParams.month && 1 + _created_date.month !== pathParams.month) {
                 return null;
             }
 
-            if (z.date && _created_date.date !== z.date) {
+            if (pathParams.date && _created_date.date !== pathParams.date) {
                 return null;
             }
 
             const path = `/blog/${_created_date.slash()}/${Component.name}`;
-            return <ArticleLine title={Component.name}
-                                key={Component.name}
-                                path={path}
-                                _created_on={Component.created_on}
-                                created_on={Component.created_on.dash()}/>
-        }).filter((x) => {
-            return x !== null
-        }).sort((a, b) => {
-            return b.props._created_on.hash() - a.props._created_on.hash()
-        }).slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE)
-        setRoutes(_routes)
-    }, [blogs, z, query]);
+            return (
+                <ArticleLine
+                    title={Component.name} key={Component.name}
+                    path={path} _created_on={Component.created_on}
+                    created_on={Component.created_on.dash()}
+                />
+            );
+        }).filter(
+            x => x
+        ).sort(
+            (a, b) => b.props._created_on.hash() - a.props._created_on.hash()
+        );
 
+        if (_routes.length > 0) {
+            _routes.splice(0, 0, <h3>{_routes[0].props._created_on.year}</h3>)
+        }
+
+        setRoutes(_routes);
+    }, [blogs, pathParams]);
 
     return (
         <>
-            <h3>Articles:</h3>
+            <h3 className="fw-bold">Articles:</h3>
             <div>
                 {routes.length > 0 ? routes : <span>No articles found</span>}
             </div>
