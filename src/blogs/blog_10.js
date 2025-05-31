0b6774cf07920f1f3aeef82140491c77ea5864cd
Prabodh Agarwal <agarwal_p@apple.com>
2025-05-31 20:36:25 +0530

    update the blog

--- src/blogs/blog_10.js
+++ src/blogs/blog_10.js
@@ -4,6 +4,10 @@ import {MyDate} from "../dates/util";
 export const name = "How to do LLMs for dummys";
 export const created_on = new MyDate(2025, 5, 31);
 
+const PinkEM = ({children}) => (
+    <em style={{color: '#e91e63'}}>{children}</em>
+);
+
 export const mod = () => {
     return (
         <>
@@ -17,6 +21,16 @@ export const mod = () => {
             For example, I want to figure out companies that are well positioned to take advantage of electronics component manufacturing in India.
             I will send in a query like <span className='rainbow'>"Which companies are well positioned to take advantage of electronics component manufacturing in India?"</span> and the LLM should return a list of companies that have mentioned this in their annual reports.
             🤑💰🤑
+            </p>
+            <p>
+                At this point of time, I am aware of the following keywords:
+                <ul>
+                    <li><PinkEM>LLM</PinkEM> - <span>Large Language Model</span></li>
+                    <li><PinkEM>Embedding</PinkEM> - <span>A way to represent text in a vector space</span></li>
+                    <li><PinkEM>Vector Database</PinkEM> - <span>A database that stores vectors</span></li>
+                    <li><PinkEM>Retrieval Augmented Generation (RAG)</PinkEM> - <span>A way to use LLMs to generate text based on a retrieval system</span></li>
+                    <li><PinkEM>Prompt Engineering</PinkEM> - <span>A way to design prompts for LLMs to get the desired output</span></li>
+                </ul>
             </p>
         </>
     );
